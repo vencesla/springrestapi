501867f0fb77afc5c98443d128b1ca45fd98f35b
vencesla <admirmabiki@gamil.com>
2022-07-07 23:43:38 +0200

    implementation des méthodes

--- src/main/java/com/admir/rest/service/EmployeeServiceImpl.java
+++ src/main/java/com/admir/rest/service/EmployeeServiceImpl.java
@@ -5,8 +5,8 @@ import com.admir.rest.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl  implements EmployeeService{
@@ -17,5 +17,29 @@ public class EmployeeServiceImpl  implements EmployeeService{
     @Override
     public List<Employee> getEmployees() {
        return eRepository.findAll();
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee){
+        return eRepository.save(employee);
+    }
+
+    @Override
+    public Employee getSingleEmployee(Long id) {
+        Optional<Employee> employee = eRepository.findById(id);
+        if(employee.isPresent()){
+            return employee.get();
+        }
+        throw new RuntimeException("Aucun n'utilisateur ne correspond à cet id "+id);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        eRepository.deleteById(id);
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        return eRepository.save(employee);
     }
 }
